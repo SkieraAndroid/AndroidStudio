fd3b5cbd59ea49c855b5c04d7c47b5b73699bd12
Paweł Skierski <cobrafan126@gmail.com>
2020-05-07 12:49:45 +0200

    07.05
    Lab6

---
+++ Lab6/app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    static public SensorManager mSensorManager;
+    static List<Sensor> SensorList;
+    static final public String SENSOR_TYPE ="sensorType";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        SensorList = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        SensorFragment sensorFragment = (SensorFragment) getSupportFragmentManager().findFragmentById(R.id.sensorList);
+        sensorFragment.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showSensor(parent,position);
+            }
+        });
+
+        setContentView(R.layout.activity_main);
+    }
+
+    public void showSensor(AdapterView<?> parent, int position) {
+
+        Intent sensIntent = new Intent(this,SensorActivity.class);
+        Sensor currentSensor = (Sensor)parent.getItemAtPosition(position);
+        sensIntent.putExtra(SENSOR_TYPE,currentSensor.getType());
+        startActivity(sensIntent);
+
+    }
+
+
+}
---
+++ Lab6/app/src/main/java/com/example/lab6/SensorActivity.java
@@ -0,0 +1,433 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.dynamicanimation.animation.DynamicAnimation;
+import androidx.dynamicanimation.animation.FlingAnimation;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
+import android.graphics.Color;
+import android.graphics.RectF;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.renderscript.Sampler;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.file.Path;
+
+import static java.lang.Math.abs;
+import static java.lang.Math.min;
+
+public class SensorActivity extends AppCompatActivity implements SensorEventListener
+{
+
+    private Sensor mSensor;
+    private int sensorType;
+    private long lastUpdate = -1;
+    private TextView sensorLabelTextView;
+    private TextView sensorValuesTextView;
+    private ImageView ballImgView;
+    private ImageView moonImgView;
+    private ImageView sunImgView;
+
+
+    private int screenWidth;
+    private int screenHeight;
+    private int imgEdgeSize;
+    private boolean layoutReady;
+    private ConstraintLayout mainContainer;
+    private Path upPath;
+    private Path downPath;
+    private boolean animFlag = false;
+    private boolean isFlashOn = false;
+    private boolean hasFlash = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sensor);
+
+        sensorLabelTextView = findViewById(R.id.sensorLabel);
+        sensorValuesTextView = findViewById(R.id.sensorValues);
+        ballImgView = findViewById(R.id.ballImageView);
+        moonImgView = findViewById(R.id.moonImageView);
+        sunImgView = findViewById(R.id.sunImageView);
+
+        Intent receivedIntent = getIntent();
+        if(receivedIntent !=null)
+        {
+            sensorType = receivedIntent.getIntExtra(MainActivity.SENSOR_TYPE,-1);
+            if(sensorType != -1)
+            {
+                mSensor = MainActivity.mSensorManager.getDefaultSensor(sensorType);
+                sensorLabelTextView.setText(mSensor.getName());
+
+                if(sensorType == Sensor.TYPE_ACCELEROMETER)
+                {
+                    ballImgView.setVisibility(View.VISIBLE);
+                    moonImgView.setVisibility(View.INVISIBLE);
+                    sunImgView.setVisibility(View.INVISIBLE);
+                }
+                else if(sensorType == Sensor.TYPE_LIGHT)
+                {
+                    ballImgView.setVisibility(View.INVISIBLE);
+                    moonImgView.setVisibility(View.VISIBLE);
+                    moonImgView.setAlpha(0f);
+                    sunImgView.setVisibility(View.VISIBLE);
+                    sunImgView.setAlpha(1f);
+                }
+                else
+                {
+                    ballImgView.setVisibility(View.INVISIBLE);
+                    moonImgView.setVisibility(View.INVISIBLE);
+                    sunImgView.setVisibility(View.INVISIBLE);
+
+                    if(sensorType == Sensor.TYPE_PROXIMITY)
+                    {
+                        hasFlash = getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+                        if(!hasFlash)
+                        {
+                            Toast.makeText(this, "No flashlight!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                }
+
+            }
+            else
+            {
+                Toast.makeText(this, "Wrong sensor type",Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        layoutReady = false;
+        mainContainer = findViewById(R.id.sensor_container);
+        mainContainer.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                imgEdgeSize = sunImgView.getWidth();
+                screenWidth = mainContainer.getWidth();
+                screenHeight = mainContainer.getHeight();
+
+                float rectY = (screenHeight - imgEdgeSize)/2f;
+                float rectHeight = screenHeight - rectY - imgEdgeSize;
+                float rectWidth = min(screenWidth - imgEdgeSize,rectHeight);
+                float rectX = (screenWidth - rectWidth - imgEdgeSize)/2f;
+
+                RectF animRect = new RectF(rectX,rectY,rectX+rectWidth,rectY+rectHeight);
+
+                upPath = new Path();
+                downPath = new Path();
+
+                upPath.arcTo(animRect,90f,-180f,true);
+
+                downPath.arcTo(animRect,270f, -180f, true);
+
+                mainContainer.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                layoutReady = true;
+
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        long timeMicro;
+
+        if(lastUpdate == -1)
+        {
+            lastUpdate = event.timestamp;
+            timeMicro = 0;
+        }
+        else
+        {
+            timeMicro = (event.timestamp - lastUpdate)/1000L;
+            lastUpdate = event.timestamp;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("Time difference:").append(timeMicro).append("\u03bcs\n");
+
+        for(int i =0; i<event.values.length; i++)
+        {
+            stringBuilder.append(String.format("Val[%d]=%.4f\n",i,event.values[i]));
+        }
+
+        TextView valueTextView = findViewById(R.id.sensorValues);
+        valueTextView.setText(stringBuilder.toString());
+
+        if(layoutReady)
+        {
+            switch (sensorType)
+            {
+                case Sensor.TYPE_LIGHT:
+                    handleLightSensor(event.values[0]);
+                    break;
+                case Sensor.TYPE_PROXIMITY:
+                    handleProximitySensor(event.values[0]);
+                    break;
+                case Sensor.TYPE_ACCELEROMETER:
+                    handleAccelerationSensor(event.values[0]);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        if(mSensor != null)
+        {
+            MainActivity.mSensorManager.registerListener(this,mSensor,100000);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        if(mSensor != null)
+        {
+            MainActivity.mSensorManager.unregisterListener(this,mSensor);
+        }
+        if(isFlashOn)
+        {
+            toogleFlash(false);
+        }
+    }
+
+    private void handleLightSensor(float sensorValue)
+    {
+        if(!animFlag &&(sensorValue<100))
+        {
+            animFlag = true;
+            moonImgView.setAlpha(0f);
+            sunImgView.setAlpha(1f);
+            lightSensorAnimation(true);
+        }
+        else if(animFlag && (sensorType == Sensor.TYPE_LIGHT && sensorValue >= 100))
+        {
+            animFlag = false;
+            lightSensorAnimation(false);
+        }
+    }
+
+    private void lightSensorAnimation(boolean showMoon) {
+
+        ObjectAnimator sunAnimator;
+        ObjectAnimator moonAnimator;
+        ObjectAnimator sunFadeAnimator;
+        ObjectAnimator moonFadeAnimator;
+
+        float moonFromAlpha = showMoon ?0f:1f;
+        float moonToAlpha = showMoon ? 1f:0f;
+        float sunFromAlpha = showMoon ? 1f:0f;
+        float sunToAlpha = showMoon ? 0f:1f;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        {
+            Path moonPath = showMoon ? upPath : downPath;
+            Path sunPath = showMoon ? downPath : upPath;
+
+            moonAnimator = ObjectAnimator.ofFloat(moonImgView,View.X,View.Y, moonPath);
+            moonAnimator.setDuration(2000);
+            moonAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            moonFadeAnimator = ObjectAnimator.ofFloat(moonImgView,"alpha",moonFromAlpha, moonToAlpha);
+            moonFadeAnimator.setDuration(2000);
+            moonFadeAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            sunAnimator = ObjectAnimator.ofFloat(sunImgView,View.X,View.Y, moonPath);
+            sunAnimator.setDuration(2000);
+            sunAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            sunFadeAnimator = ObjectAnimator.ofFloat(sunImgView,"alpha",sunFromAlpha, sunToAlpha);
+            sunFadeAnimator.setDuration(2000);
+            sunFadeAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        }
+        else {
+            float moonStartY = showMoon ? screenHeight - imgEdgeSize : (screenHeight - imgEdgeSize) / 2f;
+            float moonEndY = showMoon ? (screenHeight - imgEdgeSize) / 2f : screenHeight - imgEdgeSize;
+            float sunStartY = showMoon ? screenHeight - imgEdgeSize : (screenHeight - imgEdgeSize) / 2f;
+            float sunEndY = showMoon ? (screenHeight - imgEdgeSize) / 2f : screenHeight - imgEdgeSize;
+
+            moonAnimator = ObjectAnimator.ofFloat(moonImgView,"y",moonStartY, moonEndY);
+            moonAnimator.setDuration(2000);
+            moonAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            moonFadeAnimator = ObjectAnimator.ofFloat(moonImgView,"alpha",moonFromAlpha, moonToAlpha);
+            moonFadeAnimator.setDuration(2000);
+            moonFadeAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            sunAnimator = ObjectAnimator.ofFloat(sunImgView,"y",sunStartY, sunEndY);
+            sunAnimator.setDuration(2000);
+            sunAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+
+            sunFadeAnimator = ObjectAnimator.ofFloat(sunImgView,"alpha",sunFromAlpha, sunToAlpha);
+            sunFadeAnimator.setDuration(2000);
+            sunFadeAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        }
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.play(sunAnimator).with(moonAnimator).with(sunFadeAnimator).with(moonFadeAnimator);
+        animSet.start();
+
+        int startColor = showMoon ? Color.WHITE : getResources().getColor(android.R.color.background_dark);
+        int endColor = showMoon ? getResources().getColor(android.R.color.background_dark):Color.WHITE;
+        final int labelColor = showMoon? Color.WHITE: getResources().getColor(R.color.primaryText);
+        final int valuesColor = showMoon? Color.WHITE: getResources().getColor(R.color.secondaryText);
+        final ValueAnimator colorAnimator = ValueAnimator.ofObject(new ArgbEvaluator(),startColor,endColor);
+        colorAnimator.setDuration(1000);
+
+        colorAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animator) {
+                mainContainer.setBackgroundColor((int) animator.getAnimatedValue());
+            }
+        });
+
+        colorAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                sensorValuesTextView.setTextColor(valuesColor);
+                sensorLabelTextView.setTextColor(labelColor);
+            }
+        });
+
+    }
+
+    private void handleAccelerationSensor(float sensorValue)
+    {
+        if(!animFlag)
+        {
+            if(abs(sensorValue)>1)
+            {
+                animFlag = true;
+                FlingAnimation flingX = new FlingAnimation(ballImgView, DynamicAnimation.X);
+
+                flingX.setStartVelocity(-1*sensorValue*screenWidth/2f)
+                        .setMinValue(5)
+                        .setMaxValue(screenWidth - imgEdgeSize - 5)
+                        .setFriction(1f);
+
+
+                flingX.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                    @Override
+                    public void onAnimationEnd(DynamicAnimation dynamicAnimation, boolean b, float v, float v1) {
+
+                        if(v1 != 0 )
+                        {
+                            FlingAnimation reflingX = new FlingAnimation(ballImgView, DynamicAnimation.X);
+                            reflingX.setStartVelocity(-1*v1)
+                                    .setMinValue(5).setMaxValue(screenWidth-imgEdgeSize-5).setFriction(1.25f).start();
+                            reflingX.addEndListener(new DynamicAnimation.OnAnimationEndListener() {
+                                @Override
+                                public void onAnimationEnd(DynamicAnimation animation, boolean canceled, float value, float velocity) {
+                                    animFlag = false;
+                                }
+                            });
+                        }
+                        else
+                        {
+                            animFlag = false;
+                        }
+
+
+                    }
+                });
+
+                 flingX.start();
+            }
+        }
+    }
+
+    private void handleProximitySensor (float sensorValue)
+    {
+        if(!animFlag && (sensorValue == 0))
+        {
+            animFlag = true;
+            toogleFlash(true);
+
+        }
+        else if(animFlag && sensorValue > 0)
+        {
+            toogleFlash(false);
+        }
+    }
+
+    private void toogleFlash (boolean on)
+    {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                try
+
+                    {
+                        cameraManager.setTorchMode(cameraManager.getCameraIdList()[0],on);
+                    }
+
+                catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+        }
+        else
+        {
+            Camera camera = Camera.open();
+            Camera.Parameters parameters = camera.getParameters();
+
+            if(on)
+            {
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            }
+            else
+            {
+                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            }
+            camera.setParameters(parameters);
+
+            if(on)
+            {
+                camera.startPreview();
+            }
+            else
+            {
+                camera.stopPreview();
+                camera.release();
+            }
+        }
+
+    }
+
+}
+
